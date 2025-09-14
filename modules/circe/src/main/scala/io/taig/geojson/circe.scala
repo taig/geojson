20f9b220cba890f78db1cd6333d228448c591aae
Niklas Klein <mail@taig.io>
2025-09-14 12:22:55 +0200

    implicit -> given

--- modules/circe/src/main/scala/io/taig/geojson/circe.scala
+++ modules/circe/src/main/scala/io/taig/geojson/circe.scala
@@ -8,7 +8,7 @@ import io.circe.JsonObject
 import io.circe.syntax.*
 
 trait circe:
-  implicit val decodeGeoJson: Decoder[GeoJson] = cursor =>
+  given decodeGeoJson: Decoder[GeoJson] = cursor =>
     cursor
       .get[String]("type")
       .flatMap:
@@ -16,28 +16,28 @@ trait circe:
         case Feature.Type           => cursor.as[Feature]
         case _                      => cursor.as[Geometry]
 
-  implicit val encodeGeoJson: Encoder.AsObject[GeoJson] =
+  given encodeGeoJson: Encoder.AsObject[GeoJson] =
     case geoJson: FeatureCollection => geoJson.asJsonObject
     case geoJson: Feature           => geoJson.asJsonObject
     case geoJson: Geometry          => geoJson.asJsonObject
 
-  implicit final val decodeFeatureCollection: Decoder[FeatureCollection] = cursor =>
+  given decodeFeatureCollection: Decoder[FeatureCollection] = cursor =>
     cursor.get[List[Feature]]("features").map(FeatureCollection.apply)
 
-  implicit final val encodeFeatureCollection: Encoder.AsObject[FeatureCollection] = feature =>
+  given encodeFeatureCollection: Encoder.AsObject[FeatureCollection] = feature =>
     JsonObject(
       "type" := "FeatureCollection",
       "features" := feature.features
     )
 
-  implicit final val decodeFeature: Decoder[Feature] = cursor =>
+  given decodeFeature: Decoder[Feature] = cursor =>
     for
       id <- cursor.get[Option[String]]("id")
       geometry <- cursor.get[Option[Geometry]]("geometry")
       properties <- cursor.get[Option[Map[String, String]]]("properties")
     yield Feature(id, geometry, properties)
 
-  implicit final val encodeFeature: Encoder.AsObject[Feature] = feature =>
+  given encodeFeature: Encoder.AsObject[Feature] = feature =>
     JsonObject(
       "type" := "Feature",
       "id" := feature.geometry,
@@ -45,7 +45,7 @@ trait circe:
       "properties" := feature.properties
     )
 
-  implicit final val decodeGeometry: Decoder[Geometry] = cursor =>
+  given decodeGeometry: Decoder[Geometry] = cursor =>
     cursor
       .get[String]("type")
       .flatMap:
@@ -58,7 +58,7 @@ trait circe:
         case Polygon.Type            => cursor.as[Polygon]
         case tpe                     => DecodingFailure(s"Unknown type: $tpe", cursor.downField("type").history).asLeft
 
-  implicit final val encodeGeometry: Encoder.AsObject[Geometry] =
+  given encodeGeometry: Encoder.AsObject[Geometry] =
     case geometry: GeometryCollection => geometry.asJsonObject
     case geometry: LineString         => geometry.asJsonObject
     case geometry: MultiLineString    => geometry.asJsonObject
@@ -67,13 +67,13 @@ trait circe:
     case geometry: Point              => geometry.asJsonObject
     case geometry: Polygon            => geometry.asJsonObject
 
-  implicit final val decodeGeometryCollection: Decoder[GeometryCollection] =
+  given decodeGeometryCollection: Decoder[GeometryCollection] =
     _.get[List[Geometry]]("geometries").map(GeometryCollection.apply)
 
-  implicit final val encodeGeometryCollection: Encoder.AsObject[GeometryCollection] = collection =>
+  given encodeGeometryCollection: Encoder.AsObject[GeometryCollection] = collection =>
     JsonObject("type" := GeometryCollection.Type, "geometries" := collection.geometries)
 
-  implicit final val decodeLineString: Decoder[LineString] = cursor =>
+  given decodeLineString: Decoder[LineString] = cursor =>
     cursor
       .get[List[Position]]("coordinates")
       .flatMap: coordinates =>
@@ -81,10 +81,10 @@ trait circe:
           .fromCoordinates(coordinates)
           .toRight(DecodingFailure("Invalid format", cursor.downField("coordinates").history))
 
-  implicit final val encodeLineString: Encoder.AsObject[LineString] = lineString =>
+  given encodeLineString: Encoder.AsObject[LineString] = lineString =>
     JsonObject("type" := LineString.Type, "coordinates" := lineString.toCoordinates)
 
-  implicit final val decodeMultiLineString: Decoder[MultiLineString] = cursor =>
+  given decodeMultiLineString: Decoder[MultiLineString] = cursor =>
     cursor
       .get[List[List[Position]]]("coordinates")
       .flatMap: coordinates =>
@@ -92,38 +92,38 @@ trait circe:
           .fromCoordinates(coordinates)
           .toRight(DecodingFailure("Invalid format", cursor.downField("coordinates").history))
 
-  implicit final val encodeMultiLineString: Encoder.AsObject[MultiLineString] = multiLineString =>
+  given encodeMultiLineString: Encoder.AsObject[MultiLineString] = multiLineString =>
     JsonObject("type" := MultiLineString.Type, "coordinates" := multiLineString.toCoordinates)
 
-  implicit final val decodeMultiPoint: Decoder[MultiPoint] =
+  given decodeMultiPoint: Decoder[MultiPoint] =
     _.get[List[Position]]("coordinates").map(MultiPoint.fromCoordinates)
 
-  implicit final val encodeMultiPoint: Encoder.AsObject[MultiPoint] = multiPoint =>
+  given encodeMultiPoint: Encoder.AsObject[MultiPoint] = multiPoint =>
     JsonObject("type" := MultiPoint.Type, "coordinates" := multiPoint.toCoordinates)
 
-  implicit final val decodeMultiPolygon: Decoder[MultiPolygon] =
+  given decodeMultiPolygon: Decoder[MultiPolygon] =
     _.get[List[List[List[Position]]]]("coordinates").map(MultiPolygon.fromCoordinates)
 
-  implicit final val encodeMultiPolygon: Encoder.AsObject[MultiPolygon] = multiPolygon =>
+  given encodeMultiPolygon: Encoder.AsObject[MultiPolygon] = multiPolygon =>
     JsonObject("type" := MultiPolygon.Type, "coordinates" := multiPolygon.toCoordinates)
 
-  implicit final val decodePoint: Decoder[Point] = _.get[Position]("coordinates").map(Point.apply)
+  given decodePoint: Decoder[Point] = _.get[Position]("coordinates").map(Point.apply)
 
-  implicit final val encodePoint: Encoder.AsObject[Point] = point =>
+  given encodePoint: Encoder.AsObject[Point] = point =>
     JsonObject("type" := Point.Type, "coordinates" := point.position)
 
-  implicit final val decodePolygon: Decoder[Polygon] =
+  given decodePolygon: Decoder[Polygon] =
     _.get[List[List[Position]]]("coordinates").map(Polygon.fromCoordinates)
 
-  implicit final val encodePolygon: Encoder.AsObject[Polygon] = polygon =>
+  given encodePolygon: Encoder.AsObject[Polygon] = polygon =>
     JsonObject("type" := Polygon.Type, "coordinates" := polygon.toCoordinates)
 
-  implicit final val decodePosition: Decoder[Position] = Decoder[List[Double]].emap:
+  given decodePosition: Decoder[Position] = Decoder[List[Double]].emap:
     case longitude :: latitude :: Nil              => Position(longitude, latitude, altitude = none).asRight
     case longitude :: latitude :: elevation :: Nil => Position(longitude, latitude, elevation.some).asRight
     case _                                         => "Invalid format".asLeft
 
-  implicit final val encodePosition: Encoder.AsArray[Position] =
+  given encodePosition: Encoder.AsArray[Position] =
     case Position(longitude, latitude, None)            => Vector(longitude.asJson, latitude.asJson)
     case Position(longitude, latitude, Some(elevation)) => Vector(longitude.asJson, latitude.asJson, elevation.asJson)
 
